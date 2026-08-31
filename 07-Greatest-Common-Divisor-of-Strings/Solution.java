006aaf448424c8ab0ba67824325291bda8e712d9
Pakalapati Jayasri <jayasripakalapati@gmail.com>
2026-08-31 19:11:42 +0530

    Add GCD of Strings solution

---
+++ 07-Greatest-Common-Divisor-of-Strings/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+
+        int length = gcd(str1.length(), str2.length());
+
+        return str1.substring(0, length);
+    }
+
+    private int gcd(int a, int b) {
+
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+
+        return a;
+    }
+}
